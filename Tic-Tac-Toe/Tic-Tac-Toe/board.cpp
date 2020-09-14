2b2eb07de1959fe129ac80a155b118567bcc698c
Amin Ghasembeigi <me@aminbeigi.com>
2020-09-14 16:58:58 +1000

    Add comment

--- Tic-Tac-Toe/Tic-Tac-Toe/board.cpp
+++ Tic-Tac-Toe/Tic-Tac-Toe/board.cpp
@@ -125,8 +125,8 @@ bool Board::is_winner(string player, char marker) {
 bool Board::is_tie() {
 	// is the board filled up with Xs and Os?
 	if (this->board[0][0] != ' ' && this->board[0][1] != ' ' && this->board[0][2] != ' ' 
-		&& this->board[1][0] != ' ' && this->board[1][1] != ' '&& this->board[1][2] != ' '
-		&& this->board[2][0] != ' '&& this->board[2][1] != ' '&& this->board[2][2] != ' ') {
+		&& this->board[1][0] != ' ' && this->board[1][1] != ' ' && this->board[1][2] != ' '
+		&& this->board[2][0] != ' ' && this->board[2][1] != ' ' && this->board[2][2] != ' ') {
 		cout << "The game has been tied!";
 		return true;
 	}
